e9b82836ef056eb94c07b2006a0b4cd77d1793b2
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2023-11-10 15:22:05 -0600

    answers appear in logo.svg

--- index.js
+++ index.js
@@ -1,52 +1,75 @@
-const fs = require('fs')
-const inquirer = require('inquirer')
-const {Circle, Square, Triangle} = require('./lib/shapes.js')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { Circle, Square, Triangle } = require('./lib/shapes.js');
 
 const questions = [
-{   type: 'input',
+  {
+    type: 'input',
     name: 'characters',
-    message: 'What three characters do you want use?'
-},
-
-
-{   type: 'input',
+    message: 'What three characters do you want to use?'
+  },
+  {
+    type: 'input',
     name: 'text-color',
     message: 'What color do you want your text to be?'
-},
-
-
-{   type: 'list',
+  },
+  {
+    type: 'list',
     name: 'shapes',
     message: 'What shape do you want to use?',
     choices: ['Circle', 'Square', 'Triangle']
-},
-
-
-{   type: 'input',
+  },
+  {
+    type: 'input',
     name: 'shape-color',
     message: 'What color do you want your shape to be?'
-},
+  }
+];
 
+function logoGenerate(data) {
+  const { characters, 'text-color': textColor, shapes, 'shape-color': shapeColor } = data;
 
-]
+  // Generate the SVG content based on the user's input
+  let svgContent = `<svg width="300" height="200">`;
 
+  // Add the shape based on the selected shape type
+  if (shapes === 'Circle') {
+    const circle = new Circle();
+    circle.setColor(shapeColor);
+    svgContent += circle.render();
+  } else if (shapes === 'Square') {
+    const square = new Square();
+    square.setColor(shapeColor);
+    svgContent += square.render();
+  } else if (shapes === 'Triangle') {
+    const triangle = new Triangle();
+    triangle.setColor(shapeColor);
+    svgContent += triangle.render();
+  }
 
-//Ask Questions
-inquirer.prompt([]).then((response) => {
-    console.log(response) 
-    //Write to file
-    // const responseString = 
-    // response.characters + 
-    // " : " + 
-    // response.text-color + 
-    // " : " + 
-    // response.shapes + 
-    // " : " + 
-    // response.shape-color
-    
-    fs.writefile("logo.svg", JSON.stringify(response), (err) => {
-        err ? console.log("error", err) : console.log("Success");
+  // Add the text
+  svgContent += `<text x="150" y="100" fill="${textColor}" text-anchor="middle">${characters}</text>`;
 
-        //FUTURE NOTE: LOOK THROUGH PREVIOUS EXERCISES AND FIND CLL TO COPY AND PASTE 
-    })
-})
+  svgContent += `</svg>`;
+
+  return svgContent;
+}
+
+function writeToFile(fileName, data) {
+  const content = logoGenerate(data);
+  fs.writeFile(fileName, content, function (error) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Generated logo.svg');
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    const fileName = 'logo.svg';
+    writeToFile(fileName, data);
+  });
+}
+
+init();
