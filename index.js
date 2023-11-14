757a4f70cccddcdc5fe56358b8fd6dfba75508fa
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2023-11-14 13:44:44 -0600

    changed code around so it still works but is shortened

--- index.js
+++ index.js
@@ -6,67 +6,55 @@ const questions = [
   {
     type: 'input',
     name: 'characters',
-    message: 'What three characters do you want to use?'
+    message: 'What three characters do you want to use?',
+    validate: (input) => input.length <= 3
   },
   {
     type: 'input',
-    name: 'text-color',
+    name: 'textColor',
     message: 'What color do you want your text to be?'
   },
   {
     type: 'list',
-    name: 'shapes',
+    name: 'shape',
     message: 'What shape do you want to use?',
     choices: ['Circle', 'Square', 'Triangle']
   },
   {
     type: 'input',
-    name: 'shape-color',
+    name: 'shapeColor',
     message: 'What color do you want your shape to be?'
   }
 ];
 
 function logoGenerate(data) {
-  const { characters, 'text-color': textColor, shapes, 'shape-color': shapeColor } = data;
+  const { characters, textColor, shape, shapeColor } = data;
+  const shapeInstance = new (eval(shape))();
+  shapeInstance.setColor(shapeColor);
 
-  // Generate the SVG content based on the user's input
-  let svgContent = `<svg width="300" height="200">`;
-
-  // Add the shape based on the selected shape type
-  if (shapes === 'Circle') {
-    const circle = new Circle();
-    circle.setColor(shapeColor);
-    svgContent += circle.render();
-  } else if (shapes === 'Square') {
-    const square = new Square();
-    square.setColor(shapeColor);
-    svgContent += square.render();
-  } else if (shapes === 'Triangle') {
-    const triangle = new Triangle();
-    triangle.setColor(shapeColor);
-    svgContent += triangle.render();
-  }
-
-  // Add the text
-  svgContent += `<text x="150" y="100" fill="${textColor}" text-anchor="middle">${characters}</text>`;
-
-  svgContent += `</svg>`;
+  const svgContent = `
+    <svg width="300" height="200">
+      ${shapeInstance.render()}
+      <text x="150" y="100" fill="${textColor}" text-anchor="middle">${characters}</text>
+    </svg>
+  `;
 
   return svgContent;
 }
 
 function writeToFile(fileName, data) {
   const content = logoGenerate(data);
-  fs.writeFile(fileName, content, function (error) {
+  fs.writeFile(fileName, content, (error) => {
     if (error) {
-      return console.log(error);
+      console.log(error);
+      return;
     }
     console.log('Generated logo.svg');
   });
 }
 
 function init() {
-  inquirer.prompt(questions).then(function (data) {
+  inquirer.prompt(questions).then((data) => {
     const fileName = 'logo.svg';
     writeToFile(fileName, data);
   });
